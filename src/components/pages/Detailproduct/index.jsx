495c35f7382fc22b39a1d9c623ced75f61b6ffdb
aloywen <aliianwar7@gmail.com>
2023-08-31 07:07:30 +0700

    update detail product

--- src/components/pages/Detailproduct/index.jsx
+++ src/components/pages/Detailproduct/index.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react'
-// import Data from '../../../utils/db.json'
 import { useLocation } from 'react-router-dom'
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
 export default function Index() {
     let { state } = useLocation();
-    // const detail = Data.products;
     const [detailProduct, setDetailProduct] = useState()
+    const [isLoading, setisLoading] = useState(true)
+    const [qty, setQty] = useState(1)
 
     useEffect(() => {
         const fetchData = async () => {
@@ -16,6 +16,7 @@ export default function Index() {
                 if (response.status === 200) {
                     let data = await response.json()
                     setDetailProduct(data)
+                    setisLoading(false)
                 } else {
                     console.log('ada gangguan detail product');
                 }
@@ -25,32 +26,41 @@ export default function Index() {
             }
         }
 
-
         fetchData()
-    }, [state])
+    }, [])
 
 
-    console.log(state);
+    console.log(state.kode);
     console.log(detailProduct);
 
     return (
         <div>
-            <div>
-                <div>
-                    <LazyLoadImage
-                        effect='opacity'
-                        src={detailProduct.image}
-                        alt=""
-                        style={{ objectFit: 'cover', width: 300, height: 300 }}
-                        className='rounded-t-md'
-                    />
-                </div>
+            {isLoading ? 'loading' :
+                <div className='flex justify-around md:mx-20 gap-0'>
+                    <div>
+                        <LazyLoadImage
+                            effect='opacity'
+                            src={detailProduct.image}
+                            alt=""
+                            style={{ width: '250px', objectFit: 'contain' }}
+                            className='rounded-t-md'
+                        />
+                    </div>
 
-                <div>
-                    <p>{detailProduct.title}</p>
-                    <span className='font-primary py-2 px-5 bg-secondary text-gray-700 mt-3 font-bold'>{`$. ${prodetailProductduct.price}`}</span>
+                    <div className='w-2/3 ml-6'>
+                        <p className='text-2xl font-primary text-gray-800 '>{detailProduct.title}</p>
+                        <span className='font-primary py-2 px-5 text-gray-700 mt-3 font-bold'>{`$. ${detailProduct.price}`}</span>
+                    </div>
+
+                    <div className='w-2/3'>
+                        <div className='flex'>
+                            <button className=''>-</button>
+                            <p>{qty}</p>
+                            <button className=''>+</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            }
         </div>
     )
 }
