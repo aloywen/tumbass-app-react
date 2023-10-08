82adb0ce2b4139fb68ef36a987d72ed28993db73
aloywen <aliianwar7@gmail.com>
2023-10-08 11:33:02 +0700

    update dependencies

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -6,7 +6,7 @@ export default function Index() {
     const cat = ContextDataItem()
 
 
-    console.log(cat);
+    // console.log(cat);
     return <div>
         <div className='w-screen md:-mt-2 h-14 overflow-x-auto md:h-12 bg-primary'>
             <div className='flex items-center gap-12 md:gap-16 md:justify-center md:mx-24'>
